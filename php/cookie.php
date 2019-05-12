8c0a9260cc84f73f6ecd87d4ba756c79f83b45aa
pagemarks <48759702+pagemarks@users.noreply.github.com>
2019-05-12 16:10:52 +0800

    Update cookie.php

--- php/cookie.php
+++ php/cookie.php
@@ -2,7 +2,7 @@
 function cookie($name, $val = null, $time = 0, $only=false, $domain='xxxxx.xxx', $path = '/') {
     if ($val || $time) {
         if ($time > 0) $time = time()+$time;
-        if ($time < 0) setcookie($name);
+        elseif ($time < 0) return setcookie($name);
         return setcookie($name, $val, $time, $path, $domain, false, $only);
     } else {
         return $_COOKIE[$name] ?? '';
